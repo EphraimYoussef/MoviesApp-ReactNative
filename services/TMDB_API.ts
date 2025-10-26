7dd722884750bf59f43a8a29cb662c4c21971043
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-10-26 17:03:41 +0300

    Setup TMDB API

---
+++ services/TMDB_API.ts
@@ -0,0 +1,20 @@
+import { TMDB_CONFIG as config } from './TMDB_Config';
+
+export const fetchMovies = async ({query} : {query: string}) => {
+	const endpoint = query
+	? `${config.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+	: `${config.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+	
+	const response = await fetch(endpoint, {
+		method: 'GET',
+		headers: config.headers
+	});
+
+	if (!response.ok) {
+		throw new Error("Failed to fetch movies: " + response.statusText);
+	}
+
+	const data = await response.json();
+
+	return data;
+}
