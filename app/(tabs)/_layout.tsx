15d821554afbd4d02123f2dcd147d73c9887dd64
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-10-26 03:26:14 +0300

    Layout and TabsBar V0.1

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,39 +1,29 @@
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 
 const _layout = () => {
-  return (
-    <Tabs>
-			<Tabs.Screen
-				name='index'
-				options={{
-					title: "Home",
-					headerShown: false,
-				}}
-			/>
-			<Tabs.Screen
-				name='search'
-				options={{
-					title: "Search",
-					headerShown: false,
-				}}
-			/>
-			<Tabs.Screen
-				name='profile'
-				options={{
-					title: "Profile",
-					headerShown: false,
-				}}
-			/>
-			<Tabs.Screen
-				name='saved'
-				options={{
-					title: "Saved",
-					headerShown: false,
-				}}
-			/>
-		</Tabs>
-  )
+	return (
+		<NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Icon sf="house.fill" drawable="custom_android_drawable" />
+        <Label>Home</Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="search">
+        <Icon sf="magnifyingglass" drawable="custom_android_drawable" />
+        <Label>Search</Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="profile">
+        <Icon sf="person.fill" drawable="custom_android_drawable" />
+        <Label>Profile</Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="saved">
+        <Icon sf="bookmark.fill" drawable="custom_android_drawable" />
+        <Label>Saved</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+	)
 }
 
 export default _layout
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
 
 const profile = () => {
 	return (
-		<View className="flex-1 justify-center items-center">
-			<Text className="text-2xl font-bold text-secondary" >profile</Text>
+		<View className="flex-1 bg-primary">
+			<Image source = {images.bg} className="absolute w-full z-0" />
+			<ScrollView className="flex-1 px-5" 
+			showsVerticalScrollIndicator={false} 
+			contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+			>
+			<Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+			<Text className="text-2xl font-bold text-accent mx-auto">Profile Screen</Text>
+			</ScrollView>
 		</View>
 	)
 }
