50bd597b3592c2af6a5567c98d4f327a6bd24cdb
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-10-28 03:02:05 +0300

    Update home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,61 @@
-import SearchBar from "@/components/SearchBar";
+import SearchBar from "@/atoms/SearchBar";
+import MoviesList from "@/components/MoviesList";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import useFetch from "@/hooks/useFetch";
+import { fetchMovies } from "@/services/TMDB_API";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter(); 
 
+  const { data : movies,
+    loading : moviesLoading,
+    error : moviesError 
+  } = useFetch(
+    () => fetchMovies({query: ""})
+  );
+  
+
+
+
+
 
   return (
     <View className="flex-1 bg-primary">
+
       <Image source = {images.bg} className="absolute w-full z-0" />
+
       <ScrollView className="flex-1 px-5" 
         showsVerticalScrollIndicator={false} 
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
         >
+
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar 
-            placeholder="Search movies..."
-            onPress={ () => { router.push("/search") } }
-          />
-        </View>
-        <View className="flex-1 mt-10 mb-20">
-          <Text className="text-2xl font-bold text-accent mx-auto" >Home Screen</Text>
-        </View>
+
+        <SearchBar 
+          placeholder="Search movies..."
+          onPress={ () => { router.push("/search") } }
+        />
+
+        {
+          moviesLoading ? (
+            <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center" />
+          ) 
+          : moviesError ? (
+            <Text className="text-red-500 text-center mt-10">
+              Error fetching movies: {moviesError?.message}
+            </Text> 
+          ) 
+          : (
+            <MoviesList movies={ movies.results } />
+          )
+        }
+        
       </ScrollView>
+
     </View>
+
   );
 }
