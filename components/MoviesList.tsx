f49bbf1027bf9ecf05c7646cd369dccb6a8f8882
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-10-30 04:40:25 +0300

    Update MoviesList UI

--- components/MoviesList.tsx
+++ components/MoviesList.tsx
@@ -1,18 +1,12 @@
 import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import MovieCard from '@/atoms/MovieCard'
-import { useFonts } from 'expo-font';
 
 const MoviesList = ({ movies }: { movies: any[] }) => {
-    const [fontsLoaded] = useFonts({
-        'BebasNeue-Regular': require('../assets/fonts/BebasNeue-Regular.ttf'),
-    });
-
-    if (!fontsLoaded){ return null;}
 
     return (
         <View className="flex-1 mb-20 ">
-            <Text className="text-white text-4xl mb-5" style={{ fontFamily: 'BebasNeue-Regular' }}>Latests Movies</Text>
+            <Text className="text-white text-3xl font-semibold mb-5">Latests Movies</Text>
             <FlatList
                 data={movies}
                 renderItem={({ item }) => <MovieCard movie={item} />}
