3add1282be390a15bf8120b7837026bb9afceaff
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-10-30 04:39:27 +0300

    Added fetchMoviesDetails

--- services/TMDB_API.ts
+++ services/TMDB_API.ts
@@ -18,3 +18,23 @@ export const fetchMovies = async ({query} : {query: string}) => {
 
 	return data;
 }
+
+export const fetchMovieDetails = async (movieId: string) : Promise<MovieDetails> => {
+	try {
+		const response = await fetch(`${config.BASE_URL}/movie/${movieId}?api_key=${config.API_KEY}`, {
+			method: 'GET',
+			headers: config.headers
+		});
+
+		if (!response.ok) {
+			throw new Error("Failed to fetch movie details: " + response.statusText);
+		}
+		
+		const data = await response.json();
+		return data;
+	} 
+	catch (error) {
+		console.log(error);
+		throw new Error("Failed to fetch movie details" + error);	
+	}
+}
