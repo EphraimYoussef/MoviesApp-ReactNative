4791a924eab3402de06d897e8d4e785f96bda87f
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-11-10 22:09:37 +0200

    update profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,21 +1,33 @@
-import { View, Text, Image, ScrollView } from 'react-native'
-import React from 'react'
-import { images } from '@/constants/images'
-import { icons } from '@/constants/icons'
+import { View, Text, TouchableOpacity, Image, ScrollView} from 'react-native'
+import React, { useState } from 'react'
+import AuthModal from '@/components/AuthModal'
+import { images } from '@/constants/images';
+import { icons } from '@/constants/icons';
 
 const Profile = () => {
-	return (
+	const [showAuth, setShowAuth] = useState(false);
+
+  return (
 		<View className="flex-1 bg-primary">
 			<Image source = {images.bg} className="absolute w-full z-0" />
 			<ScrollView className="flex-1 px-5" 
-			showsVerticalScrollIndicator={false} 
-			contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
-			>
-			<Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-			<Text className="text-2xl font-bold text-accent mx-auto">Profile Screen</Text>
+				showsVerticalScrollIndicator={false} 
+				contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+				>
+				<Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+				<View className='flex items-center justify-center h-60'>
+					<TouchableOpacity
+						className='bg-light-100 px-4 py-2 rounded-xl'
+						onPress={() => setShowAuth(true)}
+					>
+						<Text className='text-primary text-2xl font-semibold ' >Login / Signup</Text>
+					</TouchableOpacity>
+				</View>
+
+				<AuthModal visible={showAuth} onClose={() => setShowAuth(false)} />
 			</ScrollView>
-		</View>
-	)
+    </View>
+  );
 }
 
 export default Profile
