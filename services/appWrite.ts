b2fafb758837f4090a1cabd416ecda3726c9a647
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-11-11 14:41:42 +0200

    Added Saved Functions ( AppWrite )

--- services/appWrite.ts
+++ services/appWrite.ts
@@ -4,6 +4,7 @@ import { TMDB_CONFIG } from "@/config/TMDB_Config";
 import { fetchMovieDetails } from "./TMDB_API";
 
 
+
 const client = new Client()
   .setEndpoint(config.ENDPOINT!)
   .setProject(config.PROJECT_ID!);    
@@ -102,5 +103,114 @@ export const getTrendingSearches = async (limit: number = 10) : Promise<Trending
   } catch (error) {
     console.log(error);
     return undefined;
+  }
+}
+
+
+export const isSaved = async (movieId : string , userId : string) : Promise<boolean> => {
+  try {
+    const result = await database.listDocuments(
+      config.DATABASE_ID!, 
+      config.SAVED_COLLECTION_ID!,
+      [
+        Query.equal("movieID", movieId),
+        Query.equal("userId", userId),
+      ]
+    );
+    return result.documents.length > 0;
+  } 
+  catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+
+export const saveMovie = async (movie : Movie, userId: string) : Promise<boolean> => {
+  try {
+    const existing = await database.listDocuments(
+      config.DATABASE_ID!,
+      config.SAVED_COLLECTION_ID!,
+      [
+        Query.equal("movieID", movie.id.toString()),
+        Query.equal("userId", userId),
+      ]
+    );
+
+    if (existing.total > 0) {
+      console.log("Movie already saved by this user.");
+      return false;
+    }
+
+    await database.createDocument(
+      config.DATABASE_ID!,
+      config.SAVED_COLLECTION_ID!,
+      ID.unique(),
+      {
+        movieID: movie.id.toString(),
+        userId: userId,
+        title: movie.title,
+        posterURL: `${TMDB_CONFIG.IMAGE_BASE_URL}${movie.poster_path}`,
+        lang: movie.original_language,
+        releaseYear: movie.release_date.split('-')[0],
+        rate: (movie.vote_average / 2).toFixed(1),
+      }
+    );
+
+    console.log("Movie saved successfully!");
+    return true;
+  } 
+  catch (error) {
+    console.error("Error saving movie:", error);
+    return false;
+  }
+};
+
+export const unsaveMovie = async (movieId: string, userId: string) : Promise<boolean> => {
+  try {
+    const existing = await database.listDocuments(
+      config.DATABASE_ID!,
+      config.SAVED_COLLECTION_ID!,
+      [
+        Query.equal("movieID", movieId),
+        Query.equal("userId", userId),
+      ]
+    );
+
+    if (existing.total === 0) {
+      console.log("Movie not saved by this user.");
+      return false;
+    }
+
+    await database.deleteDocument(
+      config.DATABASE_ID!,
+      config.SAVED_COLLECTION_ID!,
+      existing.documents[0].$id
+    );
+
+    console.log("Movie unsaved successfully!");
+    return true;
+  } 
+  catch (error) {
+    console.error("Error unsaving movie:", error);
+    return false;
+  }
+};
+
+
+export const getSavedMovies = async (userId: string) : Promise<SavedMovie[]> => {
+  try {
+    const result = await database.listDocuments(
+      config.DATABASE_ID!,
+      config.SAVED_COLLECTION_ID!,
+      [
+        Query.equal("userId", userId),
+      ]
+    );
+    return result.documents as unknown as SavedMovie[];
+  } 
+  catch (error) {
+    console.error(error);
+    return [];
   }
 }
