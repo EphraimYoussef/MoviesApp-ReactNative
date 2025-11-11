720a924e5cde3740ce0987bcfad8efb88684908d
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-11-11 03:34:56 +0200

    Updated Root Layout with ClerkProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,24 +2,31 @@ import { Stack } from "expo-router";
 import "./globals.css";
 import { StatusBar, StyleSheet, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
 export default function RootLayout() {
   return (
-    <View style={styles.container}>
-      <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
+    <ClerkProvider 
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!} 
+      tokenCache={tokenCache}
+    >
+      <View style={styles.container}>
+        <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
 
-      <LinearGradient
-        colors={["rgba(0,0,0,1)", "transparent"]}
-        style={styles.shadowOverlay}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 0, y: 1 }}
-      />
+        <LinearGradient
+          colors={["rgba(0,0,0,1)", "transparent"]}
+          style={styles.shadowOverlay}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+        />
 
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="movies/[id]" />
-      </Stack>
-    </View>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="movies" />
+        </Stack>
+      </View>
+    </ClerkProvider>
   );
 }
 
